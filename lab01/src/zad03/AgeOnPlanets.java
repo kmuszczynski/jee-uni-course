982541642c4f086d5ece11823401c91ce279f220
Kap <kacperlm@gmail.com>
2021-10-26 16:33:55 +0200

    add StringBuilder in lab01/zad04, fix prints in lab01/zad03

--- lab01/src/zad03/AgeOnPlanets.java
+++ lab01/src/zad03/AgeOnPlanets.java
@@ -9,17 +9,17 @@ public class AgeOnPlanets {
     public static void main(String[] args) {
         int numberOne = 1000000000;
         int numberTwo = 100000000;
-        System.out.printf("Testing for %d and %d seconds/n", numberOne, numberTwo);
+        System.out.printf("Testing for %d and %d seconds\n", numberOne, numberTwo);
 
 
-        System.out.printf("%d seconds:/n", numberOne);
+        System.out.printf("%d seconds:\n", numberOne);
         for (Planet p : Planet.values()) {
-            System.out.printf("Your age on %s is %.2f Earth years%n", p, ageOnPlanet(p, numberOne));
+            System.out.printf("Your age on %s is %.2f Earth years\n", p, ageOnPlanet(p, numberOne));
         }
 
-        System.out.printf("%d seconds:/n", numberTwo);
+        System.out.printf("%d seconds:\n", numberTwo);
         for (Planet p : Planet.values()) {
-            System.out.printf("Your age on %s is %.2f Earth years%n", p, ageOnPlanet(p, numberTwo));
+            System.out.printf("Your age on %s is %.2f Earth years\n", p, ageOnPlanet(p, numberTwo));
         }
     }
 }
--- lab01/src/zad04/Exes.java
+++ lab01/src/zad04/Exes.java
@@ -1,26 +1,32 @@
 package zad04;
 
 public class Exes {
-    public static void printExes(int number) {
+    public static String createPattern(int number) {
+        StringBuilder pattern = new StringBuilder();
         for (int i=0; i<number; i++) {
-            System.out.println("X".repeat(i+1));
+            pattern.append("X".repeat(i+1));
+            pattern.append("\n");
         }
         for (int i=0; i<number; i++) {
-            System.out.println("X".repeat(number-i));
+            pattern.append("X".repeat(number-i));
+            pattern.append("\n");
         }
         for (int i=0; i<number; i++) {
-            System.out.println(" ".repeat(i)+"X".repeat(number-i));
+            pattern.append(" ".repeat(i)+"X".repeat(number-i));
+            pattern.append("\n");
         }
         for (int i=0; i<number; i++) {
-            System.out.println(" ".repeat(number-i-1)+"X".repeat(i+1));
+            pattern.append(" ".repeat(number-i-1)+"X".repeat(i+1));
+            pattern.append("\n");
         }
+        return String.valueOf(pattern);
     }
 
     public static void main(String[] args) {
         System.out.println("Testing for n=3");
-        printExes(3);
+        System.out.println(createPattern(3));
 
         System.out.println("Testing for n=5");
-        printExes(5);
+        System.out.println(createPattern(5));
     }
 }
