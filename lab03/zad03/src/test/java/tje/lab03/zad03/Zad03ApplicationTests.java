095117c0c7c643eec968efbef89d8dda6fe8e793
Kap <kacperlm@gmail.com>
2021-10-27 09:27:40 +0200

    add SpringBoot demo from lab03 ex03

---
+++ lab03/zad03/src/test/java/tje/lab03/zad03/Zad03ApplicationTests.java
@@ -0,0 +1,13 @@
+package tje.lab03.zad03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
